d61e24c9f3b8a1378a25130a377bd1de4a860873
Rajat Sachdeva <rajatsachdeva3105@gmail.com>
2020-07-03 22:42:41 +0530

    Update dinoGame.py

--- dinoGame.py
+++ dinoGame.py
@@ -1,6 +1,6 @@
 import pyautogui
-from PIL import Image
-import pyscreenshot as ImageGrab
+from PIL import Image, ImageGrab
+#import pyscreenshot as ImageGrab
 import time
 
 def hit(key):
@@ -8,19 +8,19 @@ def hit(key):
     return
 
 def isCollide(data):
-    for i in range(230,280):
-        for j in range(410,440):
-            if data[i,j]<100:
+    for i in range(270,310):
+        for j in range(340,379):
+            if data[i,j]<=100:
                 hit('down')
                 return
-    for i in range(230,280):
-        for j in range(440,520):
-            if data[i,j]<100:
+    for i in range(200,280):
+        for j in range(380,448):
+            if data[i,j]<=100:
                 hit('up')
                 return
     return
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     print('Hey..Dino is about to start!!')
     time.sleep(2)
     hit('up')
